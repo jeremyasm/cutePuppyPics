d50cfc8c6811297d5ff53030b49dc29424da8161
jeremyasm <liyaoxing1991@126.com>
2018-01-22 15:43:43 +0800

    Add an action sheet controller to test page

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-test',
   templateUrl: 'test.html'
 })
+
 export class TestPage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController) {
   }
 
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      // cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
